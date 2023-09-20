8f86aad24909397cee9f7c78b5cf4177d133ab10
Martin Garcia Hervas <punighervas@gmail.com>
2023-09-20 15:45:18 -0300

    Finished

--- 02-sql/homework/01 - Exercises/homework.js
+++ 02-sql/homework/01 - Exercises/homework.js
@@ -1,16 +1,16 @@
 /* ¡Escribe tus comandos en este archivo! */
 
-const ejercicio02 = '';
+const ejercicio02 = 'SELECT * FROM movies WHERE duration<90;';
 
-const ejercicio03 = '';
+const ejercicio03 = 'SELECT * FROM movies WHERE year>=1930 and year<=1940;';
 
-const ejercicio04 = '';
+const ejercicio04 = 'SELECT * FROM movies WHERE title LIKE "%til%";';
 
-const ejercicio05 = '';
+const ejercicio05 = 'SELECT * FROM movies WHERE ARRAY_LENGTH(actors, 1)=1;';
 
 const ejercicio06 = '';
 
-const ejercicio07 = '';
+const ejercicio07 = 'SELECT * FROM movies WHERE title LIKE "%Fast and%" AND year=2016;';
 
 module.exports = {
    ejercicio02,
---
+++ Movies.session.sql
@@ -0,0 +1,1 @@
+SELECT * FROM movies WHERE title LIKE "%Fast and%" AND year=2016;
